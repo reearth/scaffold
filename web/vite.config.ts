a9e1da3de6960856ef80db2c9a8004771b3f4785
rot1024 <aayhrot@gmail.com>
2025-02-26 02:06:22 +0900

    feat(web): use vite-plugin-git-revision

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import gitRevision from "vite-plugin-git-revision";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), gitRevision({})],
 });
