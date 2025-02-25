fb7c4246d99b1ddf8f194466b01a8bcd159316c2
rot1024 <aayhrot@gmail.com>
2025-02-26 03:23:57 +0900

    fix(server): remove vite-plugin-git-revision

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import gitRevision from "vite-plugin-git-revision";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), gitRevision({})],
+  plugins: [react(), tsconfigPaths()],
 });
