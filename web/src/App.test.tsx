f0a9aa3a4cf84c7120dff53258afe8fdfb0a0489
rot1024 <aayhrot@gmail.com>
2025-02-26 02:41:09 +0900

    feat(web): introduce vitest

---
+++ web/src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+// import "@testing-library/jest-dom";
+
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Vite + React");
+  expect(linkElement).toBeVisible();
+});
