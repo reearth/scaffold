45594c9ef178f97a0670523209805104296fef94
rot1024 <aayhrot@gmail.com>
2025-05-23 20:15:01 +0900

    chore(web): add i18next and graphql-codegen

---
+++ web/codegen.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+import pkgjson from "./package.json";
+
+const schema = pkgjson.graphql.schema;
+const rootGQLDirectory = "src/lib/gql/__gen__/";
+const pluginsDirectory = `${rootGQLDirectory}/plugins`;
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema,
+  documents: "src/**/*.graphql",
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    [rootGQLDirectory]: {
+      preset: "client",
+    },
+    [`${pluginsDirectory}/graphql-request.ts`]: {
+      plugins: ["typescript", "typescript-operations", "typescript-graphql-request"],
+    },
+  },
+};
+
+export default config;
