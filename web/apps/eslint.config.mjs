427b76f96c28def4bb4d31de76eab590db5af63a
rot1024 <aayhrot@gmail.com>
2025-05-28 20:11:32 +0900

    remove eslint config from apps

--- web/apps/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import config from "eslint-config-reearth";
-
-/** @type { import("eslint").Linter.Config[] } */
-export default [
-  ...config("", { reactRecommended: true }),
-  {
-    ignores: ["/dist", "/coverage", "**/__gen__"],
-  },
-];
