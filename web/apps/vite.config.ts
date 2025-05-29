b2711fe51f427e168a23da9fded2d2b79e807a69
m-abe-dev <uuu.theman.jp@gmail.com>
2025-05-29 17:46:07 +0900

    fix: import order

--- web/apps/vite.config.ts
+++ web/apps/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
